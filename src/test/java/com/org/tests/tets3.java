22e056f7df6f8ef53f6eb2ea7d238e8ee4d2490f
navaneethukumar <31232569+navaneethukumar@users.noreply.github.com>
2020-01-25 21:44:42 +0530

    Update tets3.java
    adde extra line

--- src/test/java/com/org/tests/tets3.java
+++ src/test/java/com/org/tests/tets3.java
@@ -5,6 +5,10 @@ public class tets3 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("test3");
+		System.out.println("added extra line");
+		
+		
+		
 	}
 
 }
